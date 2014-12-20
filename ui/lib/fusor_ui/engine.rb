eef3cc0313e8d14174e08866c6df3ef8d229c76f
Brad Buckingham <bbuckingham@redhat.com>
2014-12-19 20:02:31 -0500

    fusor ui - fiddling with the engine.rb

--- ui/lib/fusor_ui/engine.rb
+++ ui/lib/fusor_ui/engine.rb
@@ -2,9 +2,9 @@ module FusorUi
   class Engine < ::Rails::Engine
     engine_name 'fusor_ui'
 
-    initializer "static assets" do |app|
-      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{config.root}/public")
-    end
+#    initializer "static assets" do |app|
+#      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{config.root}/public")
+#    end
 
     initializer 'fusor_ui.register_plugin', :after=> :finisher_hook do |app|
       Foreman::Plugin.register :fusor_ui do
@@ -30,24 +30,39 @@ module FusorUi
       end
     end
 
-    initializer "fusor_ui.assets.precompile" do |app|
-      app.config.assets.precompile += %w(fusor_ui/fusor_ui.css fusor_ui/fusor_ui.js)
-    end
-
-    initializer 'fusor_ui.configure_assets', :group => :assets do
-      SETTINGS[:fusor_ui] =
-        { :assets => { :precompile => ['fusor_ui/fusor_ui.css',
-                                       'fusor_ui/fusor_ui.js'] } }
-    end
-
-#    initializer "fusor_ui.plugin", :group => :all do |app|
-#      SETTINGS[:fusor_ui] = {:assets => {}} if SETTINGS[:fusor_ui].nil?
-#
-#      SETTINGS[:fusor_ui][:assets][:precompile] = [
-#        'fusor_ui/fusor_ui.css',
-#        'fusor_ui/fusor_ui.js'
-#      ]
+#    initializer "fusor_ui.assets.precompile" do |app|
+#      app.config.assets.precompile += %w(fusor_ui/fusor_ui.css fusor_ui/fusor_ui.js)
 #    end
+
+#    initializer 'fusor_ui.configure_assets', :group => :all do
+#      SETTINGS[:fusor_ui] =
+#        { :assets => { :precompile => ['fusor_ui/fusor_ui.css',
+#                                       'fusor_ui/fusor_ui.js'] } }
+#    end
+
+    initializer 'fusor_ui.assets_dispatcher', :before => :build_middleware_stack do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{FusorUi::Engine.root}/app/assets/javascripts/fusor_ui"
+    end
+
+    initializer "fusor_ui.assets", :group => :all do |app|
+      if Rails.env.production?
+        app.config.assets.paths << "#{FusorUi::Engine.root}/vendor/assets/stylesheets/fusor_ui"
+      else
+        app.config.less.paths << "#{FusorUi::Engine.root}/vendor/assets/stylesheets/fusor_ui"
+      end
+    end
+
+    initializer "fusor_ui.plugin", :group => :all do |app|
+      SETTINGS[:fusor_ui] = {:assets => {}} if SETTINGS[:fusor_ui].nil?
+
+      SETTINGS[:fusor_ui][:assets][:precompile] = [
+        'fusor_ui/fusor-demo.css',
+        'fusor_ui/vendor-fusor.css',
+        'fusor_ui/bootstrap.css.map',
+        'fusor_ui/fusor-demo.js',
+        'fusor_ui/vendor-fusor.js'
+      ]
+    end
 
   end
 end
